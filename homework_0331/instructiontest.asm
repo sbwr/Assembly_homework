3cd7993aa4bf54632857ae3d9f6f299e55711d8f
rui wu <2443652119@qq.com>
2021-04-05 18:14:17 +0800

    commit

--- homework_0331/instructiontest.asm
+++ homework_0331/instructiontest.asm
@@ -1,12 +1,16 @@
 CODES  SEGMENT
      ASSUME    CS:CODES
-START:;相当于main函数
+START:; the main program, like main of C++
     MOV  BX,  40ABH
     ADD  BL,  09CH 
     MOV  AL,  0E5H 
     CBW 
     ADD  BH,  AL 
     ADC  AX,  20H 
-    CBW;多加一行以便调试
+    MOV  SI,  0
+    SUB  SI,  -91
+    CBW; added line to pause and watch the result of last line
+    ;至于为什么要用英语注释
+    ;y i use eng to annotate
 CODES  ENDS
     END   START
