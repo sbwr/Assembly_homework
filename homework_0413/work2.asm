9fd95a0d19b75d33cf511ff13cfd448a533a61a8
rui wu <2443652119@qq.com>
2021-04-16 21:13:01 +0800

    done nothing

--- homework_0413/work2.asm
+++ homework_0413/work2.asm
@@ -1,20 +1,19 @@
-; 示例程序：helloworld
-stack segment
+stack segment stack
     db 100 dup(0) ;定义堆栈大小 db:define byte每个操作数大小为一字节
 stack ends
-data segment
-    msg  db "Hello!",0dh,0ah,'$'';定义消息 若干个byte组成
-const = 4c00h
+data union
+B1 db 20 dup (0)
+W1 dw 10 dup (0)
 data ends
 code segment
 main proc far
 assume cs:code,ds:data,ss:stack ;定义段名称给段寄存器
 mov ax,data
 mov ds,ax
-mov ah,09h
-mov dx,offset msg
-int 21h
-mov ax,const
+
+mov ax, B1
+mov ax, w1
+
 int 21h
 main endp
 code ends
